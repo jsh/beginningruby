0f70a988750a0c7e35b5a6737294f7857a08c2b6
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2012-01-29 08:10:30 -0700

    Save text to do something with it.

--- analyzer.rb
+++ analyzer.rb
@@ -1,3 +1,9 @@
+text = ''
 line_count = 0
-File.open("oliver.txt").each { |line| line_count += 1 }
-puts "line count is #line_count"
+
+File.open("oliver.txt").each  do |line|
+	line_count += 1
+	text << line
+end
+
+puts "line count is #{line_count}"
