2271b3032db384ea4f12fa591b1007c358fc2498
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2012-01-29 08:31:01 -0700

    Count sentences and paragraphs.

--- analyzer.rb
+++ analyzer.rb
@@ -2,6 +2,8 @@ lines = File.readlines("oliver.txt")
 puts "#{lines.size} lines"
 
 text = lines.join
+puts "#{text.split(/\n\n/).length} paragraphs"
+puts "#{text.split(/\.|\?|!/).length} sentences"
 puts "#{text.split.length} words"
 puts "#{text.length} characters"
 puts "#{text.gsub(/\s+/, '').length} characters (excluding spaces)"
