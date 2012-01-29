d6efb25e363beee22b1c9626a672a825c7789178
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2012-01-29 08:37:25 -0700

    Print some summary statistics.

--- analyzer.rb
+++ analyzer.rb
@@ -1,9 +1,15 @@
 lines = File.readlines("oliver.txt")
-puts "#{lines.size} lines"
+puts "#{l=lines.size} lines"
 
 text = lines.join
-puts "#{text.split(/\n\n/).length} paragraphs"
-puts "#{text.split(/\.|\?|!/).length} sentences"
-puts "#{text.split.length} words"
-puts "#{text.length} characters"
-puts "#{text.gsub(/\s+/, '').length} characters (excluding spaces)"
+puts "#{p=text.split(/\n\n/).length} paragraphs"
+puts "#{s=text.split(/\.|\?|!/).length} sentences"
+puts "#{w=text.split.length} words"
+puts "#{c=text.length} characters"
+puts "#{nsc=text.gsub(/\s+/, '').length} characters (excluding spaces)"
+
+puts "\nAverages:\n"
+puts "#{s/p} sentences per paragraph"
+puts "#{w/s} words per sentence"
+puts"#{w/l} words per line"
+puts "#{nsc/w} characters per word"
