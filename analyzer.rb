c626f2fb5963d8a20705cfd530830bf22d15f108
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2012-01-29 09:15:35 -0700

    Analyze for stop words vs. non-stop words

--- analyzer.rb
+++ analyzer.rb
@@ -5,6 +5,11 @@ text = lines.join
 puts "#{p=text.split(/\n\n/).length} paragraphs"
 puts "#{s=text.split(/\.|\?|!/).length} sentences"
 puts "#{w=text.split.length} words"
+
+stop_words = %w{the a by on for of are with just but and to my I has some in}
+key_words = text.split.select { | word | !stop_words.include?(word) }
+puts "#{key_words.length} key words (excluding stop words)"
+
 puts "#{c=text.length} characters"
 puts "#{nsc=text.gsub(/\s+/, '').length} characters (excluding spaces)"
 
@@ -13,3 +18,5 @@ puts "#{s/p} sentences per paragraph"
 puts "#{w/s} words per sentence"
 puts"#{w/l} words per line"
 puts "#{nsc/w} characters per word"
+puts "percentage of non-stop-words to all words: #{((key_words.length.to_f/w.to_f) * 100).to_i}"
+
