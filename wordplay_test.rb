43e5524c9fef9d264a085363850d03afe83e88c8
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2012-02-06 06:55:51 -0700

    Add real tests to go with real WordPlay functionality.

--- wordplay_test.rb
+++ wordplay_test.rb
@@ -3,9 +3,18 @@ require 'test/unit'
 require 'wordplay'
 
 class TestWordPlay < Test::Unit::TestCase
-	@@sentences = ["foo", "bar"]
 	
 	def test_basic
-		assert_equal("This is a test", WordPlay.best_sentence(@@sentences, "hello"))
+		s1 = %q(
+		Hello, world. You can see this is a test!  What do you think of that?
+		)
+		# simple test
+		assert_equal( "Hello, world", WordPlay.best_sentence(s1.sentences, ["hello"]) )
+		# case-insensitivity
+		assert_equal( "What do you think of that", WordPlay.best_sentence(s1.sentences, ["what"]) )
+		# two sentences share an important word, only one has two
+		assert_equal( "You can see this is a test", WordPlay.best_sentence(s1.sentences, ["you", "can"]) )
+		# some sentences have some, important words others have others
+		assert_equal( "What do you think of that", WordPlay.best_sentence(s1.sentences, ["this", "what", "do", "hello"]) )
 	end
 end
