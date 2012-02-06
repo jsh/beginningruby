0e3c118a4ccf85bf3b23044ced5be033fd495818
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2012-02-06 06:54:51 -0700

    Make WordPlay.best_sentence do something.
    Pick a sentence with the most desired_words.
    Should probably be a way to resolve ties.

--- wordplay.rb
+++ wordplay.rb
@@ -1,6 +1,12 @@
 class WordPlay
+	$: << '.'
+	require 'wp_string'
 	
 	def self.best_sentence(sentences, desired_words)
-		"This is a test"
+		ranked_sentences = sentences.sort_by do |s|
+			s.words.length - (s.downcase.words - desired_words).length
+		end
+		ranked_sentences.last
 	end
+	
 end
