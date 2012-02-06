17f10e6d1dd750d3e5bfa04d12d472fd60dc215f
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2012-02-06 06:59:32 -0700

    Add comments.

--- wordplay.rb
+++ wordplay.rb
@@ -2,6 +2,7 @@ class WordPlay
 	$: << '.'
 	require 'wp_string'
 	
+	# pick the sentence with the highest number of desired words
 	def self.best_sentence(sentences, desired_words)
 		ranked_sentences = sentences.sort_by do |s|
 			s.words.length - (s.downcase.words - desired_words).length
--- wp_string.rb
+++ wp_string.rb
@@ -1,7 +1,9 @@
 class String
+	# split a string into an array of sentences, without the sentence terminators
 	def sentences
 		gsub(/(\n|\r)/, '').gsub(/\s+/, ' ').gsub(/\A\s*/, '').split(/[\.\!\?]+\s*/)
 	end
+	# split a string into an array of words
 	def words
 		scan(/\w[\w\-\']*/)
 	end
