b8530e200e19e361bb5dd330db2d46e5956d475a
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2012-02-07 06:54:20 -0700

    Add pronoun switching.

--- wordplay.rb
+++ wordplay.rb
@@ -10,4 +10,28 @@ class WordPlay
 		ranked_sentences.last
 	end
 	
+	def self.switch_pronouns(text)
+		t = text.gsub(/\b(I am|You are|I|you|me|my|mine|your|yours)\b/i) do |pronoun|
+			case pronoun.downcase
+				when "i am"
+					"you are"
+				when "you are"
+					"i am"
+				when "i"
+					"you"
+				when "you"
+					"me"
+				when "me"
+					"you"
+				when "my"
+					"your"
+				when "mine"
+					"yours"
+				when "your"
+					"my"
+				when "yours"
+					"mine"
+			end
+		end.sub(/^me\b/, "i")
+	end
 end
--- wordplay_test.rb
+++ wordplay_test.rb
@@ -16,5 +16,14 @@ class TestWordPlay < Test::Unit::TestCase
 		assert_equal( "You can see this is a test", WordPlay.best_sentence(s1.sentences, ["you", "can"]) )
 		# some sentences have some, important words others have others
 		assert_equal( "What do you think of that", WordPlay.best_sentence(s1.sentences, ["this", "what", "do", "hello"]) )
+	
+	
+		assert_equal( "my cat is fighting with your cat", WordPlay.switch_pronouns('Your cat is fighting with my cat') )
+		assert_equal( "your cat is fighting with me", WordPlay.switch_pronouns('My cat is fighting with you') )
+		assert_equal( "i am your robot", WordPlay.switch_pronouns('You are my robot') )
+		assert_equal( "you gave me hope", WordPlay.switch_pronouns('I gave you hope') )
+		
+	
 	end
+		
 end
