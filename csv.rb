fb25f5917ef91c5d98c0f00cda7ebc35ddce7cde
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2012-02-08 06:58:32 -0700

    Code cleanup

--- csv.rb
+++ csv.rb
@@ -1,36 +1,32 @@
 require 'csv'
-puts 'hello, world'
-#File.open('csv.txt').each do |person|
-#	puts person
-#end
-
+#
 ## this doesn't seem to work
-#CSV.foreach('csv.txt') do |person|
+#CSV.foreach('csvfile.txt') do |person|
 #	puts person.inspect
 #end
 
-people = CSV.parse(File.read('csv.txt'))
+people = CSV.parse(File.read('csvfile.txt'))
 puts people[0][0]
 puts people[1][0]
 puts people[2][0]
 
-puts CSV.read('csv.txt').inspect
+puts CSV.read('csvfile.txt').inspect
 
-people = CSV.read('csv.txt')
+people = CSV.read('csvfile.txt')
 laura = people.find { |person| person[0] =~ /laura/i }
 puts laura.inspect
 
-young_people = CSV.read('csv.txt').find_all do |p| 
+young_people = CSV.read('csvfile.txt').find_all do |p| 
 	p[3].to_i.between?(20,40)
 end
 
 puts young_people.inspect
 
-people = CSV.read('csv.txt')
+people = CSV.read('csvfile.txt')
 laura = people.find { |person| person[0] =~ /laura/i }
 laura[0] = "Lauren Smith"
 
-CSV.open('csv1.txt', 'w') do |csv|
+CSV.open('csvfile2.txt', 'w') do |csv|
 	people.each do |p|
 		csv << p
 	end
