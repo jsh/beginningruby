bf6665b30c7ccc2d172966dc1b28e85c65108f7a
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2012-02-07 20:16:53 -0700

    More csv manipulation.

--- csv.rb
+++ csv.rb
@@ -4,9 +4,9 @@ puts 'hello, world'
 #	puts person
 #end
 
-# this doesn't seem to work
-#CSV.open('csv.txt', 'r') do |person|
-#	puts person
+## this doesn't seem to work
+#CSV.foreach('csv.txt') do |person|
+#	puts person.inspect
 #end
 
 people = CSV.parse(File.read('csv.txt'))
@@ -15,3 +15,23 @@ puts people[1][0]
 puts people[2][0]
 
 puts CSV.read('csv.txt').inspect
+
+people = CSV.read('csv.txt')
+laura = people.find { |person| person[0] =~ /laura/i }
+puts laura.inspect
+
+young_people = CSV.read('csv.txt').find_all do |p| 
+	p[3].to_i.between?(20,40)
+end
+
+puts young_people.inspect
+
+people = CSV.read('csv.txt')
+laura = people.find { |person| person[0] =~ /laura/i }
+laura[0] = "Lauren Smith"
+
+CSV.open('csv1.txt', 'w') do |csv|
+	people.each do |p|
+		csv << p
+	end
+end
