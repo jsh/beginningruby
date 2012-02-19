60aa10d15fdb5bb3424e70a3240b9d7dabe8445f
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2012-02-19 15:16:12 -0700

    Move wtring extensions into a library.

--- String_extend/string_extend.rb
+++ String_extend/string_extend.rb
@@ -3,8 +3,14 @@ class String
 	def sentences
 		gsub(/(\n|\r)/, '').gsub(/\s+/, ' ').gsub(/\A\s*/, '').split(/[\.\!\?]+\s*/)
 	end
+
 	# split a string into an array of words
 	def words
 		scan(/\w[\w\-\']*/)
+	end
+
+	# find the vowels in a string
+	def vowels
+		scan(/aeiou/i)
 	end
 end
