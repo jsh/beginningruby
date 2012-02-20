c799d679192d507c149419ebb1e13fafb7e9e5ae
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2012-02-20 12:07:27 -0700

    simplify adding one by using += 1

--- Hard/ex20.rb
+++ Hard/ex20.rb
@@ -26,8 +26,8 @@ rewind(current_file)
 current_line = 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
